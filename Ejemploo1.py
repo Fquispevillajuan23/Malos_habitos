a8d55d08e563e6913a5689792b6b1b7f30057cf8
FquispeV <70190651@continental.edu.pe>
2024-04-10 17:15:49 -0500

    sea agrega el primer grupo de archivos

---
+++ Ejemploo1.py
@@ -0,0 +1,13 @@
+def sumar(x, y):
+    return x * y
+def multi(x, y):
+    return x + y
+
+a = 10
+b = 5
+
+resultado1 = sumar(a, b)
+print("El resultado es:", resultado1)
+
+resultado2 = multi(a, b)
+print("El resultado es:", resultado2)
---
+++ Ejemploo2.py
@@ -0,0 +1,9 @@
+def calcular(a, b, c):
+    return a * b + c
+
+x = 5
+y = 3
+z = 7
+
+resultado = calcular(x, y, z)
+print("El resultado es:", resultado)
